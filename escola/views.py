63a8b2d07731bd71bbc769cc6d1b2748ff44182f
JPBeckner <pedrobeckner@hotmail.com>
2021-03-17 23:43:36 -0300

    fix views

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,19 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunoSerializer, CursoSerializer
 
 
-def alunos(request):
-    if request.method == 'GET':
-        return JsonResponse({
-            'id': 1,
-            'nome': 'Beck'
-        })
+class AlunosViewSet(viewsets.ModelViewSet):
+    """
+    Exibindo todos os alunos
+    """
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """
+    Exibindo todos os cursos
+    """
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
