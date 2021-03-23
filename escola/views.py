f1263570021d145325c94d0b69ed0fbf06056f62
JPBeckner <pedrobeckner@hotmail.com>
2021-03-23 00:13:14 -0300

    add matricula view set

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from escola.models import Aluno, Curso
-from escola.serializer import AlunoSerializer, CursoSerializer
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
 
 
 class AlunosViewSet(viewsets.ModelViewSet):
@@ -17,3 +17,12 @@ class CursosViewSet(viewsets.ModelViewSet):
     """
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """
+    Listando todas as matrículas
+    """
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
